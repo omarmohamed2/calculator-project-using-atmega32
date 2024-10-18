7d23a98ce08047bbadbddfce089da577efe4080f
omarmohamed2 <omarmohamed00730@gmail.com>
2024-10-18 16:09:29 +0300

    learning

---
+++ calculator/calculator/main.c
@@ -0,0 +1,18 @@
+/*
+ * calculator.c
+ *
+ * Created: 18/10/2024 04:00:51 م
+ * Author : omar elaswany
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
